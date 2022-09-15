9fb4b3aee873c915394a81f53cd1192c4327d5d8
tracer-ics2022 <tracer@njuics.org>
2022-09-15 17:38:37 +0800

    >  compile NEMU 211250044 朱家辰 Linux zjc-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:38:37 up  1:15,  1 user,  load average: 0.03, 0.07, 0.03

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,6 +55,24 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args)
+{
+  char *arg = strtok(NULL, " ");
+	if (arg == NULL) cpu_exec(1);
+	else cpu_exec(atoi(arg));
+	return 0;
+}
+
+static int cmd_info(char *args)
+{
+	return 0;
+}
+
+static int cmd_x(char *args)
+{
+	return 0;
+}
+
 static struct {
   const char *name;
   const char *description;
@@ -63,7 +81,9 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si", "Run N instructions of the program", cmd_si },
+	{ "info", "Print the status of the program", cmd_info },
+	{ "x", "Scan the internal storage", cmd_x },
   /* TODO: Add more commands */
 
 };
