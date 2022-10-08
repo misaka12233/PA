351920a45716dec883c314178657a9930853693f
tracer-ics2022 <tracer@njuics.org>
2022-10-08 16:58:39 +0800

    >  compile NEMU 211250044 朱家辰 Linux zjc-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:58:39 up  4:18,  1 user,  load average: 0.47, 0.40, 0.43

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -44,7 +44,10 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  char *st = s;
+  for (int i = 0; i < n; i++)
+    st[i] = c;
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -52,7 +55,11 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  //char *from = in;
+  char *to = out;
+  for (int i = 0; i < n; i++)
+    to[i] = *((char*)(in + i));
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
