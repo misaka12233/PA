5bb03e15709d57b1140fedcf2fedbf7dfee835bb
tracer-ics2022 <tracer@njuics.org>
2022-10-10 16:01:08 +0800

    >  compile NEMU 211250044 朱家辰 Linux zjc-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:01:08 up  1:58,  1 user,  load average: 0.30, 0.33, 0.37

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -58,8 +58,11 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   #ifdef CONFIG_MTRACE
-    if (CONFIG_MTRACE_START <= addr && addr <= CONFIG_MTRACE_END)
-    printf("read  : from 0x%08x, length is %d\n", addr, len);
+    if (MATRACE)
+    {
+      if (CONFIG_MTRACE_START <= addr && addr <= CONFIG_MTRACE_END)
+      printf("read  : from 0x%08x, length is %d\n", addr, len);
+    }
   #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -69,8 +72,11 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_MTRACE
-    if (CONFIG_MTRACE_START <= addr && addr <= CONFIG_MTRACE_END)
-    printf("write : from 0x%08x, length is %d\n", addr, len);
+    if (MATRACE)
+    {
+      if (CONFIG_MTRACE_START <= addr && addr <= CONFIG_MTRACE_END)
+      printf("write : from 0x%08x, length is %d\n", addr, len);
+    }
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
