7f36d913c16f3c26aeb63f753f6c471531d86d3e
tracer-ics2022 <tracer@njuics.org>
2022-10-10 11:09:42 +0800

    >  compile NEMU 211250044 朱家辰 Linux zjc-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:09:42 up  1:12,  1 user,  load average: 0.47, 0.41, 0.31

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -9,12 +9,61 @@ int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
+void sprint_int(char *out, size_t *len, int val)
+{
+  if (val == 0) return;
+  sprint_int(out, len, val / 10);
+  out[*len] = val % 10;
+  (*len)++;
+}
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  size_t i, j;
+  i = j = 0;
+  while (fmt[i] != '\0')
+  {
+    if (fmt[i] == '%')
+    {
+      i++;
+      switch (fmt[i])
+      {
+        case 'd':
+          int x = va_arg(ap, int);
+          if (x != 0)
+            sprint_int(out, &j, x);
+          else
+          {
+            out[j] = '0';
+            j++;
+          }
+          break;
+        case 's':
+          char *s = va_arg(ap, char *);
+          j += strlen(s);
+          strcat(out, s);
+          break;
+        default:
+          return -1;
+          break;
+      }
+      i++;
+    }
+    else
+    {
+      out[j] = fmt[i];
+      i++, j++;
+    }
+  }
+  return i;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int return_val = vsprintf(out, fmt, ap);
+  va_end(ap);
+  assert(return_val != -1);
+  return return_val;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
