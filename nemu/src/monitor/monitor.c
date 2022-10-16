8e5c110b4d89321eff1ccc19874e8126e9a5854e
tracer-ics2022 <tracer@njuics.org>
2022-10-16 22:24:58 +0800

    >  compile NEMU 211250044 朱家辰 Linux zjc-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:24:58 up  5:53,  1 user,  load average: 0.53, 0.37, 0.32

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -119,14 +119,14 @@ static int parse_args(int argc, char *argv[]) {
   const struct option table[] = {
     {"batch"    , no_argument      , NULL, 'b'},
     {"log"      , required_argument, NULL, 'l'},
-    {"ftrace"   , required_argument, NULL, 'f'},
+    //{"ftrace"   , required_argument, NULL, 'f'},
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
     {"help"     , no_argument      , NULL, 'h'},
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:f:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
