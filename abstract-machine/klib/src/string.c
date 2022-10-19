646c5ea6fea5159225c13f5636a2398e5f464527
tracer-ics2022 <tracer@njuics.org>
2022-10-19 17:28:50 +0800

    >  compile NEMU 211250044 朱家辰 Linux zjc-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:28:50 up  4:25,  1 user,  load average: 0.39, 0.32, 0.25

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -31,7 +31,9 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  for(size_t i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+  size_t len1 = strlen(s1);
+  size_t len2 = strlen(s2);
+  for(size_t i = 0; i < len1 || i < len2; i++)
     if (s1[i] != s2[i])
       return s1[i] - s2[i];
   return 0;
